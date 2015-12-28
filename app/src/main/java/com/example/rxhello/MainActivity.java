d00bb83bea6fb6ddc9b8494ff8b2cd050807f299
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2015-12-28 17:18:27 +0900

    use butterknife

--- app/src/main/java/com/example/rxhello/MainActivity.java
+++ app/src/main/java/com/example/rxhello/MainActivity.java
@@ -3,31 +3,24 @@ package com.example.rxhello;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_SUB = 1;
 
-    private Button mPushMeButton;
-    private TextView mTextResult;
+    @Bind(R.id.text_result)
+    protected TextView mTextResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mPushMeButton = (Button)findViewById(R.id.button_push_me);
-        mTextResult = (TextView)findViewById(R.id.text_result);
-
-        mPushMeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onPushMeClicked();
-            }
-        });
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -39,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void onPushMeClicked() {
+    @OnClick(R.id.button_push_me)
+    protected void onPushMeClicked() {
         Intent intent = new Intent(this, SubActivity.class);
         startActivityForResult(intent, REQUEST_SUB);
     }
--- app/src/main/java/com/example/rxhello/SubActivity.java
+++ app/src/main/java/com/example/rxhello/SubActivity.java
@@ -3,50 +3,36 @@ package com.example.rxhello;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SubActivity extends AppCompatActivity {
 
     public static final String BUNDLE_RESULT = "result";
 
-    private EditText mEditInput;
-    private Button mOkButton;
-    private Button mCancelButton;
+    @Bind(R.id.edit_input)
+    protected EditText mEditInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
-
-        mEditInput = (EditText)findViewById(R.id.edit_input);
-        mOkButton = (Button)findViewById(R.id.button_ok);
-        mCancelButton = (Button)findViewById(R.id.button_cancel);
-
-        mOkButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onOkClicked();
-            }
-        });
-
-        mCancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onCancelClicked();
-            }
-        });
+        ButterKnife.bind(this);
     }
 
-    private void onOkClicked() {
+    @OnClick(R.id.button_ok)
+    protected void onOkClicked() {
         Intent data = new Intent();
         data.putExtra(BUNDLE_RESULT, mEditInput.getText().toString());
         setResult(RESULT_OK, data);
         finish();
     }
 
-    private void onCancelClicked() {
+    @OnClick(R.id.button_cancel)
+    protected void onCancelClicked() {
         setResult(RESULT_CANCELED);
         finish();
     }
